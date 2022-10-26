ac3a99825c7e5d328040c136430fe1d162515359
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-26 14:59:47 +0200

    add bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,11 @@ const { createApp } = Vue
 createApp({
     data(){
         return{
-            message: 'Hello World'
+            message: 'Hello World',
+            image:'https://picsum.photos/id/684/600/400'
         }
     }
 }).mount('#app')
+
+
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
